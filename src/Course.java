00d7af94a62a59747ac4d930ec44c483373c5550
Scott <scott.lavelle@hotmail.com>
2017-02-15 20:20:58 -0500

    Course class added, changs made to Student and Instructor classes.

---
+++ src/Course.java
@@ -0,0 +1,15 @@
+/**
+ * Created by scott on 2017-02-15.
+ */
+public class Course {
+
+    String cname;
+    String description;
+
+    public Course (String cname, String description){
+
+        this.cname = cname;
+        this.description = description;
+
+    }
+}
